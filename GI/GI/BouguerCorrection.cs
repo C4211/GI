8230a4ba28fabe102a5ee2c63801fb4d69110448
iFzzzh <ifzzzh@gmail.com>
2015-07-11 15:58:58 +0800

    add 自空 布格 工具类

---
+++ GI/GI/BouguerCorrection.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GI
+{
+    class BouguerCorrection
+    {
+        #region 属性 各种路径
+        /// <summary>
+        /// exe路径
+        /// </summary>
+        public static string exePath = @"FA2BA_F.exe";
+        /// <summary>
+        /// TC.inp临时文件路径
+        /// </summary>
+        public static string tcPath = @"TC.inp";
+        /// <summary>
+        /// 站点文件(*.dat)路径
+        /// </summary>
+        public static string datPath = @"obs1_FA.dat";
+        /// <summary>
+        /// 内区地形数据文件(*.grd)路径
+        /// </summary>
+        public static string srtm30GrdPath = @"srtm30.grd";
+        /// <summary>
+        /// 外区地形数据文件(*.grd)路径
+        /// </summary>
+        public static string srtm60GrdPath = @"srtm60.grd";
+        /// <summary>
+        /// 输出文件(*.dat)路径
+        /// </summary>
+        public static string outPath = @"out.DAT";
+        /// <summary>
+        /// 存放结果
+        /// </summary>
+        public static string result;
+        #endregion
+
+        /// <summary>
+        /// 执行布格改正exe
+        /// </summary>
+        /// <param name="dat">站点文件(*.dat)</param>
+        /// <param name="srtm30">内区地形数据文件(*.grd)</param>
+        /// <param name="srtm60">外区地形数据文件(*.grd)</param>
+        /// <param name="density">密度</param>
+        /// <param name="innerRadius">内区半径</param>
+        /// <param name="outterRadius">外区半径</param>
+        /// <returns>异步执行Task</returns>
+        /// 
+        public static Task Start(string dat, string srtm30, string srtm60, double density, double innerRadius, double outterRadius)
+        {
+            //判断传入文件是否存在
+            if (!File.Exists(dat))
+                throw new Exception("站点文件不存在！");
+            if (!File.Exists(srtm30))
+                throw new Exception("内区地形数据文件不存在！");
+            if (!File.Exists(srtm60))
+                throw new Exception("外区地形数据文件不存在！");
+            //如果输出文件不存在则自动创建输出文件
+            if (!File.Exists(outPath))
+                File.Create(outPath).Dispose();
+            ////如果临时文件夹不存在则自动创建临时文件夹
+            //if (!Directory.Exists(tmpPath))
+            //    Directory.CreateDirectory(tmpPath);
+            // 输入文件全部存入临时文件夹
+            File.Copy(dat, datPath, true);
+            File.Copy(srtm30, srtm30GrdPath, true);
+            File.Copy(srtm60, srtm60GrdPath, true);
+            // 构造TC.inp内容
+            string tc = String.Format("obs1_FA.dat\nsrtm30.grd\nsrtm60.grd\nout.DAT\n5 3 0 2 {0}\n20.00416666 24.99583334 97.00416666 101.9958334\n{1} {2}", density, innerRadius, outterRadius);
+            // 写入TC.inp
+            using (var writer = new StreamWriter(tcPath, false))
+            {
+                writer.Write(tc);
+            }
+            // 执行exe
+            return Task.Run(() =>
+            {
+                Process p = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo(exePath, tcPath);
+                p.StartInfo = startInfo;
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();
+                result = p.StandardOutput.ReadToEnd();
+            });
+        }
+    }
+
+}
+
---
+++ GI/GI/FreeAirCorrection.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GI
+{
+    /// <summary>
+    /// 自空校正工具类
+    /// </summary>
+    class FreeAirCorrectionStart
+    {
+        /// <summary>
+        /// 开始自空校正
+        /// </summary>
+        /// <param name="inputPath">输入文件路径</param>
+        /// <param name="outputPath">输出文件路径</param>
+        /// <param name="choice">计算方法</param>
+        /// <returns>异步执行Task</returns>
+        public static Task Start(string inputPath, string outputPath, int choice)
+        {
+            List<FreeAirCorrection> list;
+            return Task.Run(() =>
+            {
+                list = ReadAndCheckInputFormat(inputPath);
+                CalculateFreeAirAnomaly(list, choice);
+                WriteOutput(list, outputPath);
+            });
+        }
+
+        /// <summary>
+        /// 读取输入文件并检查输入格式方法
+        /// </summary>
+        /// <param name="inputPath">输入文件路径</param>
+        /// <returns>输入内容</returns>
+        static List<FreeAirCorrection> ReadAndCheckInputFormat(string inputPath)
+        {
+            List<FreeAirCorrection> result = new List<FreeAirCorrection>();
+            using (StreamReader sr = new StreamReader(inputPath, Encoding.Default))
+            {
+                string str = sr.ReadLine();
+                string[] group;
+                double longitude, latitude, height, observed;
+                for (int line = 1; !string.IsNullOrEmpty(str); line++)
+                {
+                    // 以空格、Tab、逗号分隔
+                    group = str.Split(new char[] { ' ', '\t', ',' },
+                        StringSplitOptions.RemoveEmptyEntries);
+                    // 检查参数数量
+                    if (group.Length != 4)
+                        throw new Exception(string.Format("第{0}行参数数量错误！", line));
+                    // 格式检查
+                    if (!(double.TryParse(group[0], out longitude) && longitude >= -180.0 && longitude <= 180.0))
+                        throw new Exception("经度格式错误！");
+                    if (!(double.TryParse(group[1], out latitude) && latitude >= -90.0 && latitude <= 90.0))
+                        throw new Exception("纬度格式错误！");
+                    if (!double.TryParse(group[2], out height))
+                        throw new Exception("高度格式错误！");
+                    if (!double.TryParse(group[3], out observed))
+                        throw new Exception("测量值格式错误！");
+                    // 保存
+                    result.Add(new FreeAirCorrection(longitude, latitude, height, observed));
+                    str = sr.ReadLine();
+                }
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// 输出到文件
+        /// </summary>
+        /// <param name="list">自空校正对象列表</param>
+        /// <param name="outputPath">输出路径</param>
+        static void WriteOutput(List<FreeAirCorrection> list, string outputPath)
+        {
+            using (StreamWriter sw = new StreamWriter(outputPath, false, Encoding.Default))
+            {
+                foreach (var fac in list)
+                {
+                    sw.WriteLine(string.Format("{0} {1} {2} {3} {4}", fac.Longitude, fac.Latitude, fac.Height, fac.ObservedGravity, fac.FreeAirAnomaly));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 计算自空异常
+        /// </summary>
+        /// <param name="list">自空校正对象列表</param>
+        /// <param name="choice">校正方法</param>
+        static void CalculateFreeAirAnomaly(List<FreeAirCorrection> list, int choice)
+        {
+            FreeAirCorrection fac;
+            for (int i = 0; i < list.Count; i++)
+            {
+                fac = list[i];
+                // 计算正常重力
+                CalculateNormalGravity(fac, choice);
+                // 高度改正
+                DoHeightCorrection(fac);
+                // 计算自空异常
+                if (fac.ObservedGravity > 9999999.0)
+                {
+                    //fac.ObservedGravity微伽，fac.FreeAirAnomaly毫伽
+                    fac.FreeAirAnomaly = fac.ObservedGravity / 1000 + fac.HeightCorrection - fac.NormalGravity;
+                }
+                else
+                {
+                    //fac.ObservedGravity毫伽，fac.FreeAirAnomaly毫伽
+                    fac.FreeAirAnomaly = fac.ObservedGravity + fac.HeightCorrection - fac.NormalGravity;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 计算正常重力
+        /// </summary>
+        /// <param name="fac">自空校正对象</param>
+        /// <param name="choice">校正方法</param>
+        static void CalculateNormalGravity(FreeAirCorrection fac, int choice)
+        {
+            double a = 0.0, b = 0.0, c = 0.0;
+            switch (choice)
+            {
+                case 1://(1)1901-1909年的赫尔默特(R.Helment)公式
+                    a = 978030.00; b = 0.005302; c = 0.000007;
+                    break;
+                case 2://(2)1930年卡西尼(Cassinis)国际正常重力公式
+                    a = 978049.00; b = 0.0052884; c = 0.0000059;
+                    break;
+                case 3://(3)1971年国际正常重力公式
+                    a = 978031.80; b = 0.0053024; c = 0.0000059;
+                    break;
+                case 4://(4)1979年IUGG确定的正常重力公式
+                    a = 978032.70; b = 0.0053024; c = 0.000005;
+                    break;
+                default:
+                    throw new Exception("公式编号不正确！");
+            }
+            fac.NormalGravity = a * (1 + b * Math.Sin(Radian(fac.Latitude)) * Math.Sin(Radian(fac.Latitude)) - c * Math.Sin(2 * Radian(fac.Latitude)));
+        }
+
+        /// <summary>
+        /// 高度校正
+        /// </summary>
+        /// <param name="fac">自空校正对象</param>
+        static void DoHeightCorrection(FreeAirCorrection fac)
+        {
+            fac.HeightCorrection = 0.3086 * (1 + 0.0007 * Math.Cos(2 * Radian(fac.Latitude))) * fac.Height - 7.2e-8 * fac.Height * fac.Height;
+        }
+
+        /// <summary>
+        /// 角度转弧度
+        /// </summary>
+        /// <param name="degree">角度</param>
+        /// <returns>弧度</returns>
+        static double Radian(double degree)
+        {
+            return degree * Math.PI / 180;
+        }
+
+    }
+
+    /// <summary>
+    /// 自空类
+    /// </summary>
+    class FreeAirCorrection
+    {
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double Height { get; set; }
+        public double ObservedGravity { get; set; }
+        public double NormalGravity { get; set; }
+        public double HeightCorrection { get; set; }
+        public double FreeAirAnomaly { get; set; }
+        public FreeAirCorrection(double longitude, double latitude, double height, double observed)
+        {
+            this.Longitude = longitude;
+            this.Latitude = latitude;
+            this.Height = height;
+            this.ObservedGravity = observed;
+        }
+    }
+
+
+}
