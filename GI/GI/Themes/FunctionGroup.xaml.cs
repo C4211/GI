d6c9ea004c5d0e5436523829ae5b611a8afba5ef
iFzzzh <ifzzzh@gmail.com>
2015-07-15 22:38:28 +0800

    add some dependency properties in FunctionGroup control

--- GI/GI/Themes/FunctionGroup.xaml.cs
+++ GI/GI/Themes/FunctionGroup.xaml.cs
@@ -32,16 +32,51 @@ namespace GI
             set { SetValue(IconProperty, value); }
         }
 
-        public string Title
+        public string TitleText
         {
-            get { return (string)GetValue(TitleProperty); }
-            set { SetValue(TitleProperty, value); }
+            get { return (string)GetValue(TitleTextProperty); }
+            set { SetValue(TitleTextProperty, value); }
         }
 
-        public string Summary
+        public FontFamily TitleFontFamily
         {
-            get { return (string)GetValue(SummaryProperty); }
-            set { SetValue(SummaryProperty, value); }
+            get { return (FontFamily)GetValue(TitleFontFamilyProperty); }
+            set { SetValue(TitleFontFamilyProperty, value); }
+        }
+
+        public double TitleFontSize
+        {
+            get { return (double)GetValue(TitleFontSizeProperty); }
+            set { SetValue(TitleFontSizeProperty, value); }
+        }
+
+        public SolidColorBrush TitleForeground
+        {
+            get { return (SolidColorBrush)GetValue(TitleFontFamilyProperty); }
+            set { SetValue(TitleForegroundProperty, value); }
+        }
+
+        public string SummaryText
+        {
+            get { return (string)GetValue(SummaryTextProperty); }
+            set { SetValue(SummaryTextProperty, value); }
+        }
+
+        public FontFamily SummaryFontFamily
+        {
+            get { return (FontFamily)GetValue(SummaryFontFamilyProperty); }
+            set { SetValue(SummaryFontFamilyProperty, value); }
+        }
+
+        public double SummaryFontSize
+        {
+            get { return (double)GetValue(SummaryFontSizeProperty); }
+            set { SetValue(SummaryFontSizeProperty, value); }
+        }
+        public SolidColorBrush SummaryForeground
+        {
+            get { return (SolidColorBrush)GetValue(TitleFontFamilyProperty); }
+            set { SetValue(SummaryForegroundProperty, value); }
         }
 
         public Visibility Bd
@@ -52,10 +87,26 @@ namespace GI
         #endregion
 
         #region 注册依赖属性
-        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(DrawingBrush), typeof(FunctionGroup));
-        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(FunctionGroup));
-        public static readonly DependencyProperty SummaryProperty = DependencyProperty.Register("Summary", typeof(string), typeof(FunctionGroup));
-        public static readonly DependencyProperty BdProperty = DependencyProperty.Register("Bd", typeof(Visibility), typeof(FunctionGroup), new PropertyMetadata(Visibility.Collapsed));
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(DrawingBrush), typeof(FunctionGroup));
+        public static readonly DependencyProperty TitleTextProperty =
+            DependencyProperty.Register("TitleText", typeof(string), typeof(FunctionGroup));
+        public static readonly DependencyProperty TitleFontFamilyProperty =
+            DependencyProperty.Register("TitleFontFamily", typeof(FontFamily), typeof(FunctionGroup));
+        public static readonly DependencyProperty TitleFontSizeProperty =
+            DependencyProperty.Register("TitleFontSize", typeof(double), typeof(FunctionGroup));
+        public static readonly DependencyProperty TitleForegroundProperty =
+            DependencyProperty.Register("TitleForeground", typeof(SolidColorBrush), typeof(FunctionGroup));
+        public static readonly DependencyProperty SummaryTextProperty =
+            DependencyProperty.Register("SummaryText", typeof(string), typeof(FunctionGroup));
+        public static readonly DependencyProperty SummaryFontFamilyProperty =
+            DependencyProperty.Register("SummaryFontFamily", typeof(FontFamily), typeof(FunctionGroup));
+        public static readonly DependencyProperty SummaryFontSizeProperty =
+            DependencyProperty.Register("SummaryFontSize", typeof(double), typeof(FunctionGroup));
+        public static readonly DependencyProperty SummaryForegroundProperty =
+            DependencyProperty.Register("SummaryForeground", typeof(SolidColorBrush), typeof(FunctionGroup));
+        public static readonly DependencyProperty BdProperty =
+            DependencyProperty.Register("Bd", typeof(Visibility), typeof(FunctionGroup), new PropertyMetadata(Visibility.Collapsed));
         #endregion
     }
 }
