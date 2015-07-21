3acc5cab67839f01ac0655287c15e9d8a146fb1b
Paperman <984507810@qq.com>
2015-07-21 16:59:28 +0800

    添加页层级控制，以及页面切换动画

--- GI/GI/MainWindow.xaml.cs
+++ GI/GI/MainWindow.xaml.cs
@@ -16,16 +16,89 @@ using System.Windows.Shapes;
 
 namespace GI
 {
+
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            Page pHome = new Page(home, (string)Application.Current.Resources["GI.Detail.Name.Cn"], (string)Application.Current.Resources["GI.Detail.Name.En"]);
+            contentStack.Push(pHome);
         }
 
+        #region 返回
+        /// <summary>
+        /// 返回层级栈
+        /// </summary>
+        private Stack<Page> contentStack = new Stack<Page>();
+        /// <summary>
+        /// 当前层级关闭动画
+        /// </summary>
+        private Storyboard sbBackward = (Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Backward"];
+        /// <summary>
+        /// 上一层级返回动画
+        /// </summary>
+        private Storyboard sbBack = (Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Back"];
+        /// <summary>
+        /// 返回不可用动画
+        /// </summary>
+        private Storyboard sbBackDefault = (Storyboard)Application.Current.Resources["GI.Head.Back.Storybord.Default"];
+        /// <summary>
+        /// 返回按钮点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Group_Back(object sender, MouseButtonEventArgs e)
+        {
+            if (contentStack.Count > 1)
+            {
+                Grid current = contentStack.Pop().Grid;
+                Grid prev = contentStack.Peek().Grid;
+                current.BeginStoryboard(sbBackward);
+                prev.BeginStoryboard(sbBack);
+                ChangeHeadTitle(contentStack.Peek().Title, contentStack.Peek().Subtile);
+                ((Rectangle)sender).Fill = (DrawingBrush)Application.Current.Resources["GI.Window.Head.Back.Defalut"];
+            }
+            if (contentStack.Count <= 1)
+                headLogo.BeginStoryboard(sbBackDefault);
+        }
+        /// <summary>
+        /// 阻止返回按钮事件冒泡到标题栏
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Preview_Group_Back(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            ((Rectangle)sender).Fill = (DrawingBrush)Application.Current.Resources["GI.Window.Head.Back.Press"];
+        }
+        /// <summary>
+        /// 鼠标进入变成hover样式
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Group_Back_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((Rectangle)sender).Fill = (DrawingBrush)Application.Current.Resources["GI.Window.Head.Back.Hover"];
+            if(e.LeftButton == MouseButtonState.Pressed)
+                ((Rectangle)sender).Fill = (DrawingBrush)Application.Current.Resources["GI.Window.Head.Back.Press"];
+        }
+        /// <summary>
+        ///  鼠标离开恢复默认样式
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Group_Back_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((Rectangle)sender).Fill = (DrawingBrush)Application.Current.Resources["GI.Window.Head.Back.Default"];
+        }
+        #endregion
+
+        #region 窗体状态控制
         /// <summary>
         /// 实现顶栏可拖动
         /// </summary>
@@ -53,79 +126,137 @@ namespace GI
         {
             this.WindowState = WindowState.Minimized;
         }
+        #endregion
+
+        #region 主页按钮控制
+        /// <summary>
+        /// 返回变为可用动画
+        /// </summary>
+        private Storyboard sbBackEnable = (Storyboard)Application.Current.Resources["GI.Head.Back.Storybord.Enable"];
+        /// <summary>
+        /// 当前层级前进动画
+        /// </summary>
+        private Storyboard sbForward = (Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Forward"];
+        /// <summary>
+        /// 下一层级打开动画
+        /// </summary>
+        private Storyboard sbOpen = (Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Open"];
+        /// <summary>
+        /// 打开输入Group
+        /// </summary>
+        /// <param name="sender">需要打开的Group</param>
+        private void Group_Open(Page sender)
+        {
+            contentStack.Peek().Grid.BeginStoryboard(sbForward);
+            sender.Grid.BeginStoryboard(sbOpen);
+            contentStack.Push(sender);
+            ChangeHeadTitle(sender.Title, sender.Subtile);
+            if (contentStack.Count > 1)
+                headLogo.BeginStoryboard(sbBackEnable);
+        }
         /// <summary>
         /// 重力异常改正点击事件
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void Group1_Click(object sender, MouseButtonEventArgs e)
+        private void Group1_Open(object sender, MouseButtonEventArgs e)
         {
-            home.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Forward"]);
-            group1.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Open"]);
+            Group_Open(new Page(group1, "重力异常改正", "Gravity Exception Correction"));
         }
+        /// <summary>
+        /// 重力数据处理点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Group2_Open(object sender, MouseButtonEventArgs e)
+        {
+            Group_Open(new Page(group2, "重力数据处理", "Gravity Data Processing"));
+        }
+        /// <summary>
+        /// 重力正反演计算点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Group3_Open(object sender, MouseButtonEventArgs e)
+        {
+            Group_Open(new Page(group3,"重力正反演计算","Gravity forward calculation"));
+        }
+        /// <summary>
+        /// 重力数据解释点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Group4_Open(object sender, MouseButtonEventArgs e)
+        {
+            Group_Open(new Page(group4, "重力数据解释","Gravity data interpretation"));
+        }
+        /// <summary>
+        /// 地质体参数计算点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Group5_Open(object sender, MouseButtonEventArgs e)
+        {
+            Group_Open(new Page(group5, "地质体参数计算","Calculation of geological parameters"));
+        }
+        /// <summary>
+        /// Grd画图点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Group6_Open(object sender, MouseButtonEventArgs e)
+        {
+            Group_Open(new Page(group6, "GRD画图","Grd drawing"));
+        }
+        #endregion
 
-        private void Group1_Back(object sender, RoutedEventArgs e)
+        #region 标题栏控制
+        /// <summary>
+        /// 标题消失动画
+        /// </summary>
+        private Storyboard sbTitleDisable = (Storyboard)Application.Current.Resources["GI.Head.Title.Storybord.Disable"];
+        /// <summary>
+        /// 标题出现动画
+        /// </summary>
+        private Storyboard sbTitleDefault = (Storyboard)Application.Current.Resources["GI.Head.Title.Storybord.Default"];
+        /// <summary>
+        /// 修改标题栏文字
+        /// </summary>
+        /// <param name="title">主标题</param>
+        /// <param name="subtitle">副标题</param>
+        private void ChangeHeadTitle(string title,string subtitle)
         {
-            group1.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Backward"]);
-            home.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Back"]);
+            Storyboard sbTitleDisable_ = sbTitleDisable.Clone();
+            sbTitleDisable_.Completed += delegate
+            {
+                headTitle.Text = title;
+                headSubTitle.Text = subtitle;
+                headTitle.BeginStoryboard(sbTitleDefault);
+                headSubTitle.BeginStoryboard(sbTitleDefault);
+            };
+            headTitle.BeginStoryboard(sbTitleDisable_, HandoffBehavior.Compose, true);
+            headSubTitle.BeginStoryboard(sbTitleDisable_, HandoffBehavior.Compose, true);
         }
+        #endregion
 
-        private void Group2_Click(object sender, MouseButtonEventArgs e)
-        {
-            home.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Forward"]);
-            group2.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Open"]);
-        }
 
-        private void Group2_Back(object sender, RoutedEventArgs e)
-        {
-            group2.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Backward"]);
-            home.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Back"]);
-        }
 
-        private void Group3_Click(object sender, MouseButtonEventArgs e)
-        {
-            home.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Forward"]);
-            group3.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Open"]);
-        }
-
-        private void Group3_Back(object sender, RoutedEventArgs e)
-        {
-            group3.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Backward"]);
-            home.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Back"]);
-        }
-        private void Group4_Click(object sender, MouseButtonEventArgs e)
-        {
-            home.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Forward"]);
-            group4.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Open"]);
-        }
-
-        private void Group4_Back(object sender, RoutedEventArgs e)
-        {
-            group4.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Backward"]);
-            home.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Back"]);
-        }
-        private void Group5_Click(object sender, MouseButtonEventArgs e)
-        {
-            home.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Forward"]);
-            group5.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Open"]);
-        }
-
-        private void Group5_Back(object sender, RoutedEventArgs e)
-        {
-            group5.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Backward"]);
-            home.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Back"]);
-        }
-
-        private void Group6_Click(object sender, MouseButtonEventArgs e)
-        {
-            home.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Forward"]);
-            group6.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Open"]);
-        }
-
-        private void Group6_Back(object sender, RoutedEventArgs e)
-        {
-            group6.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Backward"]);
-            home.BeginStoryboard((Storyboard)Application.Current.Resources["GI.Body.Content.Storyboard.Back"]);
-        }
     }
+    #region 页面信息类
+    /// <summary>
+    /// 页面信息类
+    /// </summary>
+    public class Page
+    {
+        public Page(Grid page,string title,string subtitle)
+        {
+            Grid = page;
+            Title = title;
+            Subtile = subtitle;
+        }
+        public Grid Grid { get; set; }
+        public string Title { get; set; }
+        public string Subtile { get; set; }
+    }
+    #endregion
 }
