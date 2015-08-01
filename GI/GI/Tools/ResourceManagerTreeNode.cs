2c0d6dfd72e713d53e9503687fae96c4dd943656
Paperman <984507810@qq.com>
2015-08-02 03:26:25 +0800

    添加资源管理器 treenode类和默认样式

---
+++ GI/GI/Tools/ResourceManagerTreeNode.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GI.Tools
+{
+    public class ResourceManagerTreeNode : TreeViewItem
+    {
+        public ResourceManagerTreeNode()
+        {
+            Children = new List<ResourceManagerTreeNode>();
+            Path = new Path();
+            ItemLeft = 15;
+        }
+
+        #region 获取设置依赖属性
+        /// <summary>
+        /// 层级从0开始
+        /// </summary>
+        public int Level
+        {
+            get { return (int)GetValue(LevelProperty); }
+            set { SetValue(LevelProperty, value); }
+        }
+        public List<ResourceManagerTreeNode> Children { get; set; }
+
+        public Path Path { get; set; }
+        public bool HasChildren
+        {
+            get { return Children.Count == 0 ? false : true; }
+        }
+
+        public string Title {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public DrawingBrush Icon
+        {
+            get { return (DrawingBrush)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+        public GridLength LevelLeft
+        {
+            get { return new GridLength(this.Level*ItemLeft); }
+        }
+        public int ItemLeft { get; set; }
+        #endregion
+
+        #region 注册依赖属性
+        public static readonly DependencyProperty LevelProperty =
+            DependencyProperty.Register("Level",typeof(int),typeof(ResourceManagerTreeNode),new PropertyMetadata(0));
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(ResourceManagerTreeNode));
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(DrawingBrush), typeof(ResourceManagerTreeNode));
+        #endregion
+    }
+
+    public class ResourceManagerToggleButton : ToggleButton
+    {
+        public ResourceManagerToggleButton()
+        {
+        }
+        #region 获取设置依赖属性
+        /// <summary>
+        /// 层级从0开始
+        /// </summary>
+        public double IcoAngle
+        {
+            get { return (double)GetValue(IcoAngleProperty); }
+            set { SetValue(IcoAngleProperty, value); }
+        }
+        #endregion
+
+        #region 注册依赖属性
+        public static readonly DependencyProperty IcoAngleProperty =
+            DependencyProperty.Register("IcoAngle", typeof(double), typeof(ResourceManagerToggleButton));
+        #endregion
+    }
+}
