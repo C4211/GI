30b8978a89f934819b92ff5b6faadb4278b103c7
iFzzzh <ifzzzh@gmail.com>
2015-08-10 02:14:04 +0800

    完成自空

--- GI/GI/UserControls/Function_zkgz.xaml.cs
+++ GI/GI/UserControls/Function_zkgz.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -26,6 +27,91 @@ namespace GI.UserControls
             InitializeComponent();
             this.titleCn = "自空改正";
             this.titleEn = "Free Air Correction";
+        }
+
+        /// <summary>
+        /// 0 : 输入文件
+        /// 1 : 输入参数
+        /// </summary>
+        private int CurrentState = 0;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentState == 0)
+            {
+                next.IsEnabled = false;
+                CurrentState = 1;
+                content.Children[CurrentState].Visibility = Visibility.Visible;
+                Storyboard sb = ((Storyboard)this.FindResource("sb")).Clone();
+                ((ThicknessAnimation)sb.Children[0]).To = new Thickness(-CurrentState * 680, 0, 0, 0);
+                sb.Completed += delegate { content.Children[CurrentState - 1].Visibility = Visibility.Hidden; next.IsEnabled = true; };
+                content.BeginStoryboard(sb);
+                prev.Visibility = Visibility.Visible;
+                next.Content = "计算";
+                return;
+            }
+            else if (CurrentState == 1)
+            {
+                DoFreeAirCorrection();
+            }
+        }
+
+        private void prev_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (CurrentState > 0)
+            {
+                next.IsEnabled = false;
+                CurrentState -= 1;
+                content.Children[CurrentState].Visibility = Visibility.Visible;
+                Storyboard sb = ((Storyboard)this.FindResource("sb")).Clone();
+                ((ThicknessAnimation)sb.Children[0]).To = new Thickness(-CurrentState * 680, 0, 0, 0);
+                sb.Completed += delegate { content.Children[CurrentState + 1].Visibility = Visibility.Hidden; next.IsEnabled = true; };
+                content.BeginStoryboard(sb);
+                if (CurrentState <= 0)
+                    prev.Visibility = Visibility.Hidden;
+                next.Content = "下一步";
+                next.Visibility = Visibility.Visible;
+            }
+        }
+        private async void DoFreeAirCorrection()
+        {
+            string inPath = inputPath1.filePath.Text;
+            string outPath = outputPath1.filePath.Text;
+            int choice = 1;
+            if (!FileNameFilter.CheckFileSuffix(inPath))
+                Msg("输入文件类型不正确！");
+            else if (!FileNameFilter.CheckFileExistence(inPath))
+                Msg("输入文件路径不存在！");
+            else
+            {
+                try
+                {
+                    if (choice1.IsChecked == true)
+                        choice = 1;
+                    else if (choice2.IsChecked == true)
+                        choice = 2;
+                    else if (choice2.IsChecked == true)
+                        choice = 3;
+                    else if (choice2.IsChecked == true)
+                        choice = 4;
+                    task = null;
+                    task = FreeAirCorrectionStart.Start(inPath, outPath, choice);
+                    await task;
+                    Msg("计算完成！");
+                }
+                catch (Exception e)
+                {
+                    Msg(e.Message);
+                }
+            }
+        }
+
+        private Task task = null;
+
+        private void Msg(string msg)
+        {
+            Dispatcher.Invoke(delegate { MessageBox.Show(Application.Current.MainWindow, msg); });
         }
     }
 }
