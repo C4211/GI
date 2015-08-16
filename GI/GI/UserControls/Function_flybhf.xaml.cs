5af8469aaca599aa96091e91c7e61e16a8ad2ac8
Paperman <984507810@qq.com>
2015-08-16 19:15:19 +0800

    添加傅里叶变换法  删除部分冗余代码

--- GI/GI/UserControls/Function_flybhf.xaml.cs
+++ GI/GI/UserControls/Function_flybhf.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -26,6 +27,79 @@ namespace GI.UserControls
             InitializeComponent();
             this.titleCn = "傅里叶变换法";
             this.titleEn = "Fourier Transform Method";
+        }
+        /// <summary>
+        /// 0 : 输入文件
+        /// 1 : 输入参数
+        /// 2 : 计算中
+        /// </summary>
+        private int CurrentState = 0;
+        private bool IsCanceled = false;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentState == 0)
+            {
+                content.IsEnabled = false;
+                buttons.IsEnabled = false;
+                CurrentState = 1;
+                content.Children[CurrentState].Visibility = Visibility.Visible;
+                Storyboard sb = ((Storyboard)this.FindResource("sb")).Clone();
+                ((ThicknessAnimation)sb.Children[0]).To = new Thickness(-CurrentState * 680, 0, 0, 0);
+                sb.Completed += delegate { content.Children[CurrentState - 1].Visibility = Visibility.Hidden; content.IsEnabled = true; buttons.IsEnabled = true; };
+                content.BeginStoryboard(sb);
+                prev.Visibility = Visibility.Visible;
+                next.Content = "计算";
+                return;
+            }
+            else if (CurrentState == 1)
+            {
+                CurrentState = 2;
+                IsCanceled = false;
+                next.Content = "取消";
+                //计算
+            }
+            else if (CurrentState == 2)
+            {
+                CurrentState = 1;
+                next.Content = "计算";
+                //停止计算
+            }
+        }
+
+        private void prev_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (CurrentState > 0)
+            {
+                next.IsEnabled = false;
+                CurrentState -= 1;
+                content.Children[CurrentState].Visibility = Visibility.Visible;
+                Storyboard sb = ((Storyboard)this.FindResource("sb")).Clone();
+                ((ThicknessAnimation)sb.Children[0]).To = new Thickness(-CurrentState * 680, 0, 0, 0);
+                sb.Completed += delegate { content.Children[CurrentState + 1].Visibility = Visibility.Hidden; next.IsEnabled = true; };
+                content.BeginStoryboard(sb);
+                if (CurrentState <= 0)
+                    prev.Visibility = Visibility.Hidden;
+                next.Content = "下一步";
+                next.Visibility = Visibility.Visible;
+            }
+        }
+        private void HidePrevAndCancel()
+        {
+            Dispatcher.Invoke(delegate
+            {
+                prev.Visibility = Visibility.Hidden;
+                cancel.Visibility = Visibility.Hidden;
+            });
+        }
+        private void ShowPrevAndCancel()
+        {
+            Dispatcher.Invoke(delegate
+            {
+                prev.Visibility = Visibility.Visible;
+                cancel.Visibility = Visibility.Visible;
+            });
         }
     }
 }
