16ce2ac92d82fbc1297e75665dc19c9318cd3da0
iFzzzh <ifzzzh@gmail.com>
2016-03-19 16:39:13 +0800

    modify InterfaceInversion to original version

--- GI/GI/Functions/ZLZFYJS/InterfaceInversion.cs
+++ GI/GI/Functions/ZLZFYJS/InterfaceInversion.cs
@@ -15,7 +15,7 @@ namespace GI.Functions
         /// <summary>
         /// exe路径
         /// </summary>
-        public static string exePath = @"bin\InterfaceInversion.exe";
+        public static string exePath = @"bin\Moho3.exe";
         /// <summary>
         /// parameters.inp临时文件路径
         /// </summary>
