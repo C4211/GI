930413848644eb41398576e005cb0c35cea11d96
Paperman <984507810@qq.com>
2016-03-30 14:10:32 +0800

    添加缩放平移

--- GI/GI/Tools/GRDPreviewWindow.xaml.cs
+++ GI/GI/Tools/GRDPreviewWindow.xaml.cs
@@ -117,7 +117,7 @@ namespace GI.Tools
         private RenderTargetBitmap grdbmp;
         private void SaveImage(FrameworkElement ui)
         {
-            grdbmp = new RenderTargetBitmap((int)ui.ActualWidth, (int)ui.ActualHeight, 96d, 96d,
+            grdbmp = new RenderTargetBitmap((int)ui.Width, (int)ui.Height, 96d, 96d,
                 PixelFormats.Pbgra32);
             grdbmp.Render(ui);
         }
@@ -203,8 +203,110 @@ namespace GI.Tools
         {
             Dispatcher.Invoke(delegate { MessageWindow.Show(Application.Current.MainWindow, msg); });
         }
-        
 
-        
+        #region 放大功能
+
+        private bool mouseDown;
+        private Point mouseXY;
+        private double min = 0.1, max = 3.0;//最小/最大放大倍数
+
+
+        private void Domousemove(Canvas img, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                return;
+            }
+            var group = grdContent.FindResource("TfGroup1") as TransformGroup;
+            var transform = group.Children[1] as TranslateTransform;
+            var transform2 = group.Children[0] as ScaleTransform;
+            //获取拖拽后的鼠标坐标
+            var position = e.GetPosition(fileContentGrid);
+            //修改图片偏移量
+            transform.X -= mouseXY.X - position.X;
+            transform.Y -= mouseXY.Y - position.Y;
+            mouseXY = position;
+        }
+
+
+        private void DowheelZoom(TransformGroup group, Point point, double delta)
+        {
+            //修改缩放中心(跟随鼠标）
+            var pointToContent = group.Inverse.Transform(point);
+            var transform = group.Children[0] as ScaleTransform;
+            //控制图片缩放倍数
+            var transform1 = group.Children[1] as TranslateTransform;
+            if (transform.ScaleX + delta < min)
+            {
+                transform.ScaleX = min;
+                transform.ScaleY = min;
+            }
+            else if (transform.ScaleX + delta > max)
+            {
+                transform.ScaleX = max;
+                transform.ScaleY = max;
+            }
+            else
+            {
+                transform.ScaleX += delta;
+                transform.ScaleY += delta;
+            }
+            transform1.X = -1 * ((pointToContent.X * transform.ScaleX) - point.X);
+            transform1.Y = -1 * ((pointToContent.Y * transform.ScaleY) - point.Y);
+        }
+
+
+        private void ContentControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var img = sender as Canvas;
+            if (img == null)
+            {
+                return;
+            }
+            img.CaptureMouse();
+            mouseDown = true;
+            //获取点击时的鼠标坐标
+            mouseXY = e.GetPosition(fileContentGrid);
+        }
+
+        private void ContentControl_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var img = sender as Canvas;
+            if (img == null)
+            {
+                return;
+            }
+            img.ReleaseMouseCapture();
+            mouseDown = false;
+        }
+
+        private void ContentControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            var img = sender as Canvas;
+            if (img == null)
+            {
+                return;
+            }
+            if (mouseDown)
+            {
+                Domousemove(img, e);
+            }
+        }
+
+        private void ContentControl_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var img = sender as Canvas;
+            if (img == null)
+            {
+                return;
+            }
+            var point = e.GetPosition(img);
+            var group = grdContent.FindResource("TfGroup1") as TransformGroup;
+            var delta = e.Delta * 0.001;
+            DowheelZoom(group, point, delta);
+        }
+
+        #endregion
+
     }
 }
