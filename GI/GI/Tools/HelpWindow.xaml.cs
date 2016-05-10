c3bec29b7342cd15415a795f969a15f1605661ff
Paperman <984507810@qq.com>
2016-05-10 23:14:07 +0800

    添加帮助

---
+++ GI/GI/Tools/HelpWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace GI.Tools
+{
+    /// <summary>
+    /// HelpWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class HelpWindow : Window
+    {
+        public HelpWindow(Window owner)
+        {
+            InitializeComponent();
+            this.Owner = owner;
+        }
+
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private bool isClosed = false;
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (isClosed == false)
+            {
+                e.Cancel = true;
+                Storyboard sb = (this.FindResource("GI.Window.closeStoryboard") as Storyboard).Clone();
+                sb.Completed += delegate { isClosed = true; this.Close(); };
+                content.BeginStoryboard(sb);
+            }
+            else
+            {
+                e.Cancel = false;
+            }
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            Storyboard sb = (this.FindResource("GI.Window.Border.Focus") as Storyboard).Clone();
+            content.BeginStoryboard(sb);
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            Storyboard sb = (this.FindResource("GI.Window.Border.Default") as Storyboard).Clone();
+            content.BeginStoryboard(sb);
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                Storyboard sb = (this.FindResource("GI.Window.openStoryboard") as Storyboard).Clone();
+                content.BeginStoryboard(sb);
+            }
+        }
+
+        private void Head_Min_Click(object sender, RoutedEventArgs e)
+        {
+            Storyboard sb = (this.FindResource("GI.Window.closeStoryboard") as Storyboard).Clone();
+            sb.Completed += delegate { this.WindowState = WindowState.Minimized; };
+            content.BeginStoryboard(sb);
+        }
+
+        public void Min()
+        {
+            Storyboard sb = (this.FindResource("GI.Window.closeStoryboard") as Storyboard).Clone();
+            sb.Completed += delegate { this.WindowState = WindowState.Minimized; };
+            content.BeginStoryboard(sb);
+        }
+
+        public void Normal()
+        {
+            this.WindowState = WindowState.Normal;
+        }
+    }
+}
--- GI/GI/UserControls/FunctionPages/FunctionPage_home.xaml.cs
+++ GI/GI/UserControls/FunctionPages/FunctionPage_home.xaml.cs
@@ -34,5 +34,12 @@ namespace GI.UserControls
             MessageWindow.Show(Application.Current.MainWindow, "版权所有：\n中国科学院测量与地球物理研究所");
         }
 
+        private void Help_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            HelpWindow hw = new HelpWindow(Application.Current.MainWindow);
+            hw.Show();
+        }
+
+        
     }
 }
